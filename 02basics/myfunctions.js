426bece8840c9220593b0d499720b3432bb4e2d5
Aashiq Ahmed M <ahmed.aashiq@saama.com>
2022-11-08 13:21:10 +0000

    functions with arguments in ts

--- 02basics/myfunctions.js
+++ 02basics/myfunctions.js
@@ -1,12 +1,20 @@
 "use strict";
 exports.__esModule = true;
 function addTwo(num) {
-    // return num + 2
-    return "hello";
+    return num + 2;
+    //return "hello"
 }
+function addThree(num) {
+    return num + 3;
+}
+var numaddedThree = addThree(4);
 function getUpper(val) {
     return val.toUpperCase();
 }
+function getLower(val) {
+    return val.toLowerCase();
+}
+var lowercaseValue = getLower("SOME");
 function signUpUser(name, email, isPaid) { }
 var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
@@ -15,3 +23,23 @@ var myValue = addTwo(5);
 getUpper("hitesh");
 signUpUser("hitesh", "hitesh@lco.dev", false);
 loginUser("h", "h@h.com");
+// function getValue(myVal: number): boolean{
+//     if (myVal > 5) {
+//         return true
+//     }
+//     return "200 OK"
+// }
+var getHello = function (s) {
+    return "";
+};
+var heros = ["thor", "spiderman", "ironman"];
+// const heros = [1, 2, 3]
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
--- 02basics/myfunctions.ts
+++ 02basics/myfunctions.ts
@@ -1,21 +1,33 @@
-function addTwo(num: number): number{
-     return num + 2
-    //return "hello"
+function addTwo(num: number): number {
+  return num + 2;
+  //return "hello"
 }
 
-function getUpper(val: string){
-    return val.toUpperCase()
+function addThree(num: number) {
+  return num + 3;
 }
 
-function signUpUser(name: string, email: string, isPaid: boolean){}
+let numaddedThree = addThree(4);
 
-let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
 
-let myValue = addTwo(5)
-getUpper("hitesh")
+function getLower(val: string) {
+  return val.toLowerCase();
+}
 
-signUpUser("hitesh", "hitesh@lco.dev", false)
-loginUser("h", "h@h.com")
+let lowercaseValue = getLower("SOME");
+
+function signUpUser(name: string, email: string, isPaid: boolean) {}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
+
+let myValue = addTwo(5);
+getUpper("hitesh");
+
+signUpUser("hitesh", "hitesh@lco.dev", false);
+loginUser("h", "h@h.com");
 
 // function getValue(myVal: number): boolean{
 //     if (myVal > 5) {
@@ -24,27 +36,22 @@ loginUser("h", "h@h.com")
 //     return "200 OK"
 // }
 
-const getHello = (s: string):string => {
-    return ""
-}
+const getHello = (s: string): string => {
+  return "";
+};
 
-const heros = ["thor", "spiderman", "ironman"]
+const heros = ["thor", "spiderman", "ironman"];
 // const heros = [1, 2, 3]
 
 heros.map((hero): string => {
-    return `hero is ${hero}`
-})
+  return `hero is ${hero}`;
+});
 
-
-function consoleError(errmsg: string): void{
-    console.log(errmsg);
-    
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
 }
-function handleError(errmsg: string): never{
-    throw new Error(errmsg);
-    
+function handleError(errmsg: string): never {
+  throw new Error(errmsg);
 }
 
-
-
-export {}
+export {};
