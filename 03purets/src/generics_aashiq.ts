3cef95191e4f9c529e69e6c45144ede946162eb4
Aashiq Ahmed M <ahmed.aashiq@saama.com>
2022-12-02 23:09:18 +0530

    generics in arrow function

--- 03purets/src/generics_aashiq.ts
+++ 03purets/src/generics_aashiq.ts
@@ -8,3 +8,8 @@ type name = {
 }
 // newIdentity('12')
 newIdentity<name>({firstName: 'Aashiq', lastName: 'Ahmed'})
+
+const searchMoreTerms = <T>(val: Array<T>) : T => {
+    // do some operation
+    return val[1]
+}
